b20320fb8618fe3a3cd8d64df968aa61c436d14e
gmartpad <contato.gabrielmartinspadoin@gmail.com>
2025-07-31 19:27:36 -0300

    [NODE] STORING_DAT_IN_THE_FILES_VIA_MODEL - feat: started storing data in files via model

--- models/product.js
+++ models/product.js
@@ -1,15 +1,32 @@
-const products = []
-
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../util/path')
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    products.push(this)
+    const p = path.join(rootDir, 'data', 'products.json')
+    fs.readFile(p, (error, fileContent) => {
+      let products = [];
+      if (!error) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (error) => {
+        console.log('error: ', error)
+      })
+    })
   }
 
   static fetchAll() {
-    return products;
+    const p = path.join(rootDir, 'data', 'products.json')
+    fs.readFile(p, (error, fileContent) => {
+      if (error) {
+        return [] 
+      }
+      return JSON.parse(fileContent)
+    })
   }
 }
